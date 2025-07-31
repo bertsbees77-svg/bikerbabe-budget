56fe06de945bd56ffa5fae9ffec982e30d385957
bertsbees77-svg <bertsbees77@gmail.com>
2025-07-31 13:14:04 -0500

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Motorcycle Gear Budget App</h1>
+      <p>Welcome! Start building your dream setup.</p>
+    </div>
+  );
+}
+
+export default App;
